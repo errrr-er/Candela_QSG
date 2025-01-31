3eac53dc117dc988a36fd59b20a74e508d20409f
errrr-er <luj13014@gmail.com>
2025-01-30 23:33:28 -0500

    修改base配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress';
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
   lang: 'en-US',
+  base: '/blog/',
   title: 'VitePress',
   description: 'Vite & Vue powered static site generator.',
 
